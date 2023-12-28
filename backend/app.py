54d337b4c348747f12c48ca768bcf837ea90e5d7
arbezerra <arbezerra@gmail.com>
2023-12-28 12:22:09 -0300

    feat(backend): add backend skeleton

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from recommender import Recommender
+
+from dotenv import load_dotenv
+load_dotenv()
+
+cors = CORS()
+recommender = Recommender()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_pyfile('config.py')
+
+    cors.init_app(app)
+
+    @app.route('/healthz')
+    def index():
+        return "ok"
+
+    @app.route('/recomm', methods=['GET', 'POST'])
+    def recomm():
+        state = request.json.get('state')
+        return jsonify({"products": recommender.recommend(state)})
+
+    return app
