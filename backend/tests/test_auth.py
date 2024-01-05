a2f941ee915325944ce050daf954f88252c24b4d
arbezerra <arbezerra@gmail.com>
2024-01-05 02:24:37 -0300

    fix(tests): chose random email

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 def test_auth_login(app):
     client = app.test_client()
     response = client.post(
@@ -7,8 +10,10 @@ def test_auth_login(app):
 
 def test_auth_register(app):
     client = app.test_client()
+    email = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for _ in range(20))
     response = client.post(
         '/auth/register',
-        json={"name": "Test", "email": "user@example.com", "password": "123"}
+        json={"name": "Test", "email": f"{email}@example.com", "password": "123"}
     )
     assert response.status_code == 200
