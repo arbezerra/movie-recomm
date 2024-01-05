ee5012fdf35bb9f231863552e51a1de376689a15
arbezerra <arbezerra@gmail.com>
2024-01-05 03:20:17 -0300

    fix(tests): wait for umap before the recommendation test

--- backend/tests/test_recommender.py
+++ backend/tests/test_recommender.py
@@ -1,6 +1,10 @@
+import time
+
 
 def test_recommender(app, token):
     client = app.test_client()
+    # Wait for UMAP
+    time.sleep(30)
     response = client.post(
         '/recommend', headers={'Authorization': f"Bearer {token}"})
     assert response.status_code == 200
